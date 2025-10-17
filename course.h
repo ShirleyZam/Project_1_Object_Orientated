02241fbe393be5f26d5aae3223f31cba34f46403
ShirleyZam <shirleyzamora1018@gmail.com>
2025-10-16 20:43:05 -0400

    Update course.h

--- course.h
+++ course.h
@@ -1,16 +1,22 @@
+#include <iostream> 
 #include <string>
+
 using namespace std;
 
 class Course {
 private:
-    string name;
-    int creditHours;
-    string letterGrade;
+    string name; // gets the name of the courses
+    int creditHours; // gets the credit of the courses 
+    string letterGrade; // gets the letter grade of the courses 
 
 public:
-    // Constructor
-    Course(string courseName, int credits, string grade)
-        : name(courseName), creditHours(credits), letterGrade(grade) {}
+    // Constructor 
+    Course(string courseName, int credits, string grade) {
+    name = courseName;
+    creditHours = credits;
+    letterGrade = grade;
+}
+
 
     // Getters
     string getName() const {
