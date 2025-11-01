df15609a2f184006a27181900e95d18834c2a96e
ShirleyZam <shirleyzamora1018@gmail.com>
2025-11-01 16:41:34 -0400

    Update display function output format

--- bankingAccount.h
+++ bankingAccount.h
@@ -90,7 +90,7 @@ using namespace std;
 
                 // Person 2: Fully define this function to print owner name, account number, and balance
                 void display() const{
-                    cout << "=== Banking System ===\n";
+                    cout << "\n=== Account Info ===\n";
                     cout << "Owners Name: " << ownerName << endl; 
                     cout << "Account Number: " << accountNumber <<endl;
                     cout << "Balance: $"<< balance <<endl;
