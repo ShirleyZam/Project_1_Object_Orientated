da7ea561d4d2430e769d421eedf62b930fa0e3a0
nom9 <143737976+nom9@users.noreply.github.com>
2025-12-01 19:03:14 -0500

    Add files via upload

---
+++ Order.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class Order{
+	private:
+		string flavorname;
+		int scoops;
+		double totalprice;
+		
+		
+		
+		
+		
+		
+	public:	
+		Order();
+		Order(const string& name, int s, double tp);
+		
+		string getflavorname() const;
+		int getscoops();
+		double gettotalprice();
+		
+		void print() const;
+};
