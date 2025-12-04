3c384a3923aadfe1c05148f6c22543012b1db8b2
Lucia Zuniga <Zuniga00lucia@gmail.com>
2025-12-04 18:14:35 -0500

    Update Flavor.h

--- Flavor.h
+++ Flavor.h
@@ -7,36 +7,48 @@
 
 using namespace std;
 
+//psrent - General
 class Flavor {
 private:
-string name;
-double scoopPrice;
-int bucketSize;
+string name;         //flavor name
+double scoopPrice;   //price per scoop 
+int bucketSize;      //num of scoops in bucket
 
 public:
+//Default constructor -> "Default" placholder values 
 Flavor(){
   name= "Unknown";
   scoopPrice = 0.0;
-  bucketSize = 10;
+  bucketSize = 10; //default bucket size
 }
 
-Flavor (String n, double price,  int bucket = 10){
-name = n;
-scoopPrice = Price;
-bucketSize = 10;
+//Parameterized constructor -> allows create a Flavot with:
+//a specific name, a certain price scoop, a default bucket size of 10
+Flavor (string n, double price,  int bucket = 10){
+name = n; //Specific name
+scoopPrice = Price; //certain price scoop 
+bucketSize = 10; //default 10
 }
 
-string getName() const { return name;}
-double getPrice() const { return scoopPrice;}
-int getBucketSize() const { return bucketSize;}
+//Getters - returns private data safely 
+//allowing other parts of the program to safely (controlled access) read the private member variables of class
+//Name, scoopPrice, bucketSize = Private -> encapsulation - cannot be accessed outside the class
+string getName() const { return name;}          //return -> prevents direct modification 
+double getPrice() const { return scoopPrice;}   //won't modify member variables
+int getBucketSize() const { return bucketSize;} //read only 
 
+//-Virtual- Function -> (-allows-) child class to override (-replace-) parent (The permission)
+//Why?
 virtual double priceCalc(int scoops) const{
-  return scoops* scoopPrice;
+  return scoops* scoopPrice; //This changes.. 
 }
 
 virtual ~Flavor() = default;
 };
 
+//Child Class - (Inherits) with own special rules
+//calculates price like parent(base class) 
+//Should still be treated like Flavor
 class RegularFlavor : public Flavor{
 public:
 RegularFlavor(string n, double price, int bucket = 10)
@@ -47,6 +59,9 @@ dtring grtType() const override{
 }
 };
 
+//Child Class - (Inherits) with own special rules
+//calculates price with discount different to parent(base class) 
+//Should still be treated like Flavor
 class SeasonalFlavor : public Flavor{
 private:
 double discount;
