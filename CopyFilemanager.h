8e889f066aad6ebd946c6d04d440ffc11bdcde32
Rejuana-sys <rijuana1970@gmail.com>
2025-12-04 13:59:28 -0500

    Enhance comments in CopyFilemanager.h
    Added comments to clarify the purpose of the FileManager class methods and header guard.

--- CopyFilemanager.h
+++ CopyFilemanager.h
@@ -1,6 +1,6 @@
-#ifndef FILEMANAGER_H
+#ifndef FILEMANAGER_H // this file is uncluded but only once
 
- #define FILEMANAGER_H
+ #define FILEMANAGER_H    //define the header guard
  
 #include <string>
 
@@ -12,18 +12,19 @@
  
 using namespace std;
  
-class FileManager {
+class FileManager { // it will handle file writing and reading
 
- public:
 
-     static void saveInventory(const Inventory& data, const string& filename);
+ public:   // this can be use for outside of the functions
 
-     static void loadInventory(Inventory& data, const string& filename);
+     static void saveInventory(const Inventory& data, const string& filename); // it will save inventory data into a file
+
+     static void loadInventory(Inventory& data, const string& filename); // it will load inventory data from a file
  
-    static void appendSale(const Order& order, const string& filename);
+    static void appendSale(const Order& order, const string& filename); // it will add a new sale of the order to the file
 
-     static void loadSales(vector<Order>& orders, const string& filename);
+     static void loadSales(vector<Order>& orders, const string& filename); // here it will load all orders from a file
 
- };
+ }; // end of the filemanager class
  
-#endif // FILEMANAGER_H
+#endif // FILEMANAGER_H and close the header guard
