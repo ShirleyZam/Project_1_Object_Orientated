428c67e077787ec4ec54d585d794b2897038f738
ShirleyZam <shirleyzamora1018@gmail.com>
2025-12-03 21:19:41 -0500

    Update and rename Inventory.h to CopyInventory.h

---
+++ CopyInventory.h
@@ -0,0 +1,57 @@
+#ifndef INVENTORY_H
+
+ #define INVENTORY_H
+ 
+#include <iostream>
+
+ #include <vector>
+
+ #include <string>
+
+ #include "Flavor.h"
+
+ #include "Exceptions.h"
+
+ using namespace std;
+ 
+class Inventory {
+
+ private:
+
+     vector<Flavor*> flavors;
+
+     int* scoopsArray;
+
+     int flavorCount;
+ 
+    int findFlavorIndex(const string& name) const;
+
+     static string toLower(const string& s);
+ 
+public:
+
+     Inventory();
+
+     ~Inventory();
+ 
+    void addFlavor(Flavor* flavor, int initialScoops);
+
+     void deleteFlavor(const string& name);
+
+     double sellScoops(const string& name, int scoops);
+
+     void restock(const string& name, int scoops);
+ 
+    const Flavor* findFlavor(const string& name) const;
+
+     int getScoopsAvailable(const string& name) const;
+ 
+    int getFlavorCount() const { return flavorCount; }
+
+     const vector<Flavor*>& getFlavors() const { return flavors; }
+ 
+    friend ostream& operator<<(ostream& os, const Inventory& inv);
+
+ };
+ 
+#endif // INVENTORY_H
