09a96dbf1e6ffab2b51a8393dcc895e737e3252a
Lucia Zuniga <Zuniga00lucia@gmail.com>
2025-12-04 19:15:13 -0500

    Update Flavor.h

--- Flavor.h
+++ Flavor.h
@@ -33,9 +33,9 @@ bucketSize = 10; //default 10
 //Getters - returns private data safely 
 //allowing other parts of the program to safely (controlled access) read the private member variables of class
 //Name, scoopPrice, bucketSize = Private -> encapsulation - cannot be accessed outside the class
-string getName() const { return name;}          //return -> prevents direct modification 
-double getPrice() const { return scoopPrice;}   //won't modify member variables
-int getBucketSize() const { return bucketSize;} //read only 
+string getName() const { return name; }          //return -> prevents direct modification 
+double getPrice() const { return scoopPrice; }   //won't modify member variables
+int getBucketSize() const { return bucketSize; } //read only 
 
 //-Virtual- Function -> (-allows-) child class to override (-replace-) parent (The permission)
 //Why?
@@ -43,38 +43,56 @@ virtual double priceCalc(int scoops) const{
   return scoops* scoopPrice; //This changes.. 
 }
 
-virtual ~Flavor() = default;
-};
+//Overrides and (reveals) returns string the type of flavor (object-> seasonal or regular)
+ virtual string getType() const {
+         return "Flavor";
+ }
 
-//Child Class - (Inherits) with own special rules
-//calculates price like parent(base class) 
-//Should still be treated like Flavor
+//Destructor - delete flavor (pointer -> derived  object) - memory safety 
+    virtual ~Flavor() = default; //generate default body
+ };
+
+
+
+//Inherits from Flavor class
 class RegularFlavor : public Flavor{
 public:
-RegularFlavor(string n, double price, int bucket = 10)
-: Flavor(n,price,10){}
+//Constructor
+//Child Class - (Inherits) with own special rules 
+//calculates price like parent(base class) 
+//Should still be treated like Flavor
+RegularFlavor(string n, double price, int bucket = 10) //Parameters (What is needed)
+: Flavor(n,price,10){} //(initializer list)
 
-dtring grtType() const override{
+//replaces (Overrides - determines type of flavor) and checks (spelling, return type, parameters)
+string getType() const override{
   return "Regular";
 }
 };
 
+
+//Inherits from Flavor class
+class SeasonalFlavor : public Flavor{
+private:
+double discount; //special rule (difference)
+                 //Private-> we dont want discount to be accessible all the time or access to leak
+
+public:
+//constructor
 //Child Class - (Inherits) with own special rules
 //calculates price with discount different to parent(base class) 
 //Should still be treated like Flavor
-class SeasonalFlavor : public Flavor{
-private:
-double discount;
+seasonalFlavor(string n, double price, int bucket, double d) //Parameters (What is needed)
+: Flavor(n, price, 10), discount(d) {} //calls base (parent) constructor
+//Initializer list                     //intialize new member for this class 
 
-public:
-seasonalFlavor(string n, double price, int bucket, double d)
-: Flavor(n, price, 10), discount(d) {}
-
-double priceCalc(int scoops) const override{
-  double base = scoops*getPrice();
-return base - (base*discount);
+//Overriden priceCalc
+double priceCalc(int scoops) const override{ //replaing calculations
+  double base = scoops*getPrice(); //Calculate normal price liek parent (base class)
+return base - (base*discount); //(applies discount for seasonal flavor
 }
 
+//replaces (Overrides - determines type of flavor(seasonal)) and checks (spelling, return type, parameters)
 string getType() const override{
   return "Seasonal";
 }
