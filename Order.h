dca3665eb7f0299be2a89465422113ba2a6cf5af
nom9 <143737976+nom9@users.noreply.github.com>
2025-12-04 19:40:14 -0500

    Add files via upload

---
+++ Order.h
@@ -0,0 +1,25 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <string>
+#include <iostream>
+//Darien Hector
+using namespace std;
+
+class Order{
+	//variables to be used in the functions
+	private:
+		string flavorname;
+		int scoops;
+		double totalprice;
+	
+	//declares the functions so you are able to order
+	public:
+		Order();
+		Order(const string& name, ints, double tp);
+		string getflavorname() const;
+		int getscoops() const;
+		double gettotalprice() const;
+		void print() const;
+};
+#endif
